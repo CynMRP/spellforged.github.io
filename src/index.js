454268384691314b6ab3a40156cf7672b9991b1f
WarlordWeaponry <minecraft2013@cox.net>
2021-02-20 01:42:44 -0600

    commit attempt

---
+++ src/index.js
@@ -0,0 +1,8 @@
+﻿import './styles/site.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'jquery/dist/jquery.slim.min'
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
